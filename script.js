43e0deb59e16e0e5bea86076b951f10d65c50c05
Nate <150736820+NateHerrera@users.noreply.github.com>
2024-07-19 14:15:07 -0500

    I'm about 3/4th way of completing this current version. Still need to add more JS features and reactivity.

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("view-more-link1").addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+        alert("You're already here silly!");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("view-more-link2").addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+        alert("I'm still ongoing my coding journey!");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("view-more-link3").addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+        alert("Will be back soon!");
+    });
+});
+
